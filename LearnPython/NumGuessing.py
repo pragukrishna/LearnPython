ca0857f4119b3d8dd8f103464d4d863a5767f18b
PraKri <33298725+pragukrishna@users.noreply.github.com>
2025-11-20 01:19:35 +0000

    number guessing game

--- LearnPython/NumGuessing.py
+++ LearnPython/NumGuessing.py
@@ -1,25 +1,33 @@
+#this code is a fun number guessing game. The user will try to guessa randomly generated number betwwen 1 and 50 where they have hints if they guess a higher number or a lower number. 
+#They get 10 attempts to guess this number
+
 import random
 import time
 
 def number_guess():
-    num_to_guess = random.randint(1, 20)
+    num_to_guess = random.randint(1, 50)
+    attempts = 10
 
     print("Welcome to the number guessing game!")
-    time.sleep(3)
-    print("Guess a number between 1 and 20...")
-    time.sleep(2)
+    #time.sleep(3)
+    print("Guess a number between 1 and 50. You have 10 attempts to guess this number.")
+    #time.sleep(2)
 
     num = int(input("Enter your guess: "))
-    time.sleep(2)
-    while num != num_to_guess:
+    #time.sleep(2)
+    while attempts > 0:
         if num > num_to_guess:
-            time.sleep(1)
+            #time.sleep(1)
             print("Too high!")
-        else:
-            time.sleep(1)
+        elif num < num_to_guess:
+            #time.sleep(1)
             print("Too low!")
+        else:
+            print("Congratulations, you've guessed the number!")
+            break
+        attempts -= 1
         num = int(input("Enter your new guess: "))
-        time.sleep(2)
-    print("Congratulations, you've guessed the number!")
+    #time.sleep(2)
+    print("Sorry, you've reached the maximum number of attempts. The number was:", num_to_guess)
 
 number_guess()
